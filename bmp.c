dd84a924470e41a42cd68f3a7184f5c59318ad1a
anoban <2016sb117@univ.jfn.ac.lk>
2023-10-08 18:38:14 +0530

    modified:   bmp.c

--- bmp.c
+++ bmp.c
@@ -373,8 +373,8 @@ static inline BMP to_blacknwhite(_In_ const BMP* image, _In_ const RGBTOBWKIND c
         case BINARY :
             for (size_t i = 0; i < local.npixels; ++i) {
                 local.pixel_buffer[i].BLUE  = local.pixel_buffer[i].BLUE > 128 ? 255 : 0;
-                local.pixel_buffer[i].GREEN = local.pixel_buffer[i].BLUE > 128 ? 255 : 0;
-                local.pixel_buffer[i].RED   = local.pixel_buffer[i].BLUE > 128 ? 255 : 0;
+                local.pixel_buffer[i].GREEN = local.pixel_buffer[i].GREEN > 128 ? 255 : 0;
+                local.pixel_buffer[i].RED   = local.pixel_buffer[i].RED > 128 ? 255 : 0;
             }
             break;
         case FLATBINARY :
@@ -435,13 +435,13 @@ static void __forceinline __stdcall close_bmp(_In_ const BMP image) {
     return;
 }
 
-int wmain(void) {
+int wmain(_In_ int argc, _In_ wchar_t* argv[]) {
     // if (argc < 2) {
     //     fputs("Error: no files passed", stderr);
     //     exit(1);
     // }
 
-    BMP image   = new_bmp(L"./sydney.bmp");
+    BMP image   = new_bmp(argv[1]);
 
     BMP bw_ave  = to_blacknwhite(&image, AVERAGE, false);
     BMP bw_wav  = to_blacknwhite(&image, WEIGHTED_AVERAGE, false);
@@ -449,12 +449,12 @@ int wmain(void) {
     BMP bw_bin  = to_blacknwhite(&image, BINARY, false);
     BMP bw_fbin = to_blacknwhite(&image, FLATBINARY, false);
 
-    BMP nored   = remove_color(&image, REMRED, false);
-    BMP nogreen = remove_color(&image, REMGREEN, false);
-    BMP noblue  = remove_color(&image, REMBLUE, false);
-    BMP norg    = remove_color(&image, REMRG, false);
-    BMP norb    = remove_color(&image, REMRB, false);
-    BMP nogb    = remove_color(&image, REMGB, false);
+    // BMP nored   = remove_color(&image, REMRED, false);
+    // BMP nogreen = remove_color(&image, REMGREEN, false);
+    // BMP noblue  = remove_color(&image, REMBLUE, false);
+    // BMP norg    = remove_color(&image, REMRG, false);
+    // BMP norb    = remove_color(&image, REMRB, false);
+    // BMP nogb    = remove_color(&image, REMGB, false);
 
     serialize_bmp(&bw_ave, L"./sydney_bw_ave.bmp");
     serialize_bmp(&bw_wav, L"./sydney_bw_wav.bmp");
@@ -462,12 +462,12 @@ int wmain(void) {
     serialize_bmp(&bw_bin, L"./sydney_bw_bin.bmp");
     serialize_bmp(&bw_fbin, L"./sydney_bw_fbin.bmp");
 
-    serialize_bmp(&nored, L"./sydney_GB.bmp");
-    serialize_bmp(&noblue, L"./sydney_RG.bmp");
-    serialize_bmp(&nogreen, L"./sydney_RB.bmp");
-    serialize_bmp(&nogb, L"./sydney_R.bmp");
-    serialize_bmp(&norb, L"./sydney_G.bmp");
-    serialize_bmp(&norg, L"./sydney_B.bmp");
+    // serialize_bmp(&nored, L"./sydney_GB.bmp");
+    // serialize_bmp(&noblue, L"./sydney_RG.bmp");
+    // serialize_bmp(&nogreen, L"./sydney_RB.bmp");
+    // serialize_bmp(&nogb, L"./sydney_R.bmp");
+    // serialize_bmp(&norb, L"./sydney_G.bmp");
+    // serialize_bmp(&norg, L"./sydney_B.bmp");
 
     close_bmp(image);
 
@@ -477,12 +477,12 @@ int wmain(void) {
     close_bmp(bw_bin);
     close_bmp(bw_fbin);
 
-    close_bmp(nored);
-    close_bmp(noblue);
-    close_bmp(nogreen);
-    close_bmp(nogb);
-    close_bmp(norb);
-    close_bmp(norg);
+    // close_bmp(nored);
+    // close_bmp(noblue);
+    // close_bmp(nogreen);
+    // close_bmp(nogb);
+    // close_bmp(norb);
+    // close_bmp(norg);
 
     return EXIT_SUCCESS;
 }
