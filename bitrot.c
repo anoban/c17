0c781e36da4e4a4f49d28c7f8a13060c8a255559
anoban <2016sb117@univ.jfn.ac.lk>
2024-01-13 18:14:13 +0530

    modified:   bitrot.c

--- bitrot.c
+++ bitrot.c
@@ -61,17 +61,18 @@ static inline void __stdcall leftrotbit(
     bool leftmost = false,
          pushoff  = false; // leftmost and rightmost bits within the shift boundary i.e bitstream + offset to bitstream + offset + n
 
-    for (size_t i = 0; i < n; ++i) {                        // number of bits to rotate
+    for (size_t _ = 0; _ < n; ++_) {                        // number of bits to rotate
         for (size_t j = 0; j <= (length - 1) / 8LLU; ++j) { // number of bytes in the specified bitstream
             // captures the state of the first bit of the selected byte
             pushoff = getbit(bitstream + j, 0 /* first bit of the current byte */);
+            // wprintf_s(L"First bit if %zu th byte is %d\n", j, pushoff);
 
-            if (!j) // first bit and last bit need swapping, so store the first bit of first byte.
+            if (!j) // first bit and last bit of the stream need swapping, so store the first bit of first byte.
                 leftmost = pushoff;
             else
                 // swap the first (leftmost) bit of the current byte with the last (rightmost) bit of the previous byte.
                 setbit(bitstream + j - 1 /* picking the previous byte */, 7 /* the last bit */, pushoff);
-            bitstream[j] <<= 1;
+            bitstream[j] >>= 1; // Intel x86_64 is little endian, so we need to do a right shift for left rotation
         }
         // set the last (rightmost) bit of the rotate window to the first bit of it.
         setbit(bitstream, length - 1, leftmost);
@@ -81,16 +82,15 @@ static inline void __stdcall leftrotbit(
 }
 
 int main(void) {
-    const uint8_t refbits[] = { 0, 255 };
-    uint8_t       bits[]    = { 0, 255 };
+    const uint8_t refbits[] = { 255, 0, 0, 1 };
+    uint8_t       bits[]    = { 255, 0, 0, 1 };
     // for (size_t i = 0; i < __crt_countof(bits) * 8; ++i) wprintf_s(L"Position: %4zu, state: %2d\n", i, getbit(bits, i));
 
     // wprintf_s(L"Position: %4zu, state: %2d\n", i, getbit(bits, i));
     // setbit(bits, i, false);
-    leftrotbit(bits, sizeof(bits) * 8, 2);
+    leftrotbit(bits, sizeof(bits) * 8, 20);
     // wprintf_s(L"Position: %4zu, state: %2d\n", i, getbit(bits, i));
 
-    for (size_t i = 0; i < __crt_countof(bits) * 8; ++i)
-        wprintf_s(L"Position: %4zu, state => original: %2d, post-rotation: %2d\n", i, getbit(refbits, i), getbit(bits, i));
+    for (size_t i = 0; i < __crt_countof(bits) * 8; ++i) wprintf_s(L"%4zu: %2d %2d\n", i, getbit(refbits, i), getbit(bits, i));
     return EXIT_SUCCESS;
 }
