a7b241424371877cb02ac4e4ee07e3b732c9e005
anoban <2016sb117@univ.jfn.ac.lk>
2024-02-24 14:41:19 +0530

    modified:   c23.c 	new file:   crc32__.c 	new file:   htonl.c

--- c23.c
+++ c23.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-[[nodiscard]] uint64_t sum(uint64_t* restrict array, const uint64_t length) {
+uint64_t sum(uint64_t* restrict array, const uint64_t length) {
     uint64_t sum = 0;
     for (size_t i = 0; i < length; ++i) sum += array[i];
     return sum;
---
+++ htonl.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define _AMD64_
+#define WIN32_LEAN_AND_MEAN
+#define WIN32_EXTRA_MEAN
+#include <winsock.h>
+#include <stdlib.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main(void){
+	unsigned long number = 0x00AA00CC;
+	wprintf_s(L"number: %u, htonl(number): %u\n", number, htonl(number));
+	wprintf_s(L"number: %X, htonl(number): %X\n", number, htonl(number));
+	return EXIT_SUCCESS;
+}
