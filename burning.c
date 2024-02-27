83e596bdcf90f05293dbd6d655fa5ef695ade6bf
anoban <2016sb117@univ.jfn.ac.lk>
2024-02-27 15:06:08 +0530

    modified:   .vscode/launch.json 	modified:   .vscode/tasks.json 	modified:   burning.c 	new file:   heap.c

--- burning.c
+++ burning.c
@@ -1,183 +1,176 @@
-#define WIN32_LEAN_AND_MEAN
-#define WIN32_EXTRA_MEAN
-#ifdef __clang__
-    #define UNICODE
-#endif
 // clang-format off
-#include <Windows.h>
-#include <CommCtrl.h>
+#include <wchar.h>
+#include <windows.h>
+#include <commctrl.h>
 // clang-format on
 
-#pragma comment(lib, "User32.lib")
 #pragma comment(lib, "Gdi32.lib")
-#pragma comment(lib, "Comctl32.lib")
+#pragma comment(lib, "IPHlpApi.lib")
+#pragma comment(lib, "User32.lib")
+#pragma comment(lib, "comctl32.lib")
 
-HINSTANCE        hGlobalInstance;
-LRESULT          rGlobalPosition = 200;
+LRESULT CALLBACK PanelProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-LRESULT CALLBACK PanelProcedure(_In_ HWND hWindow, _In_ UINT uMessage, _In_ WPARAM wParameter, _In_ LPARAM lParameter) {
-    HBRUSH                      hYellowBrush, hRedBrush, hOldBrush;
-    HPEN                        hPen, hOldPen;
-    HFONT                       hFont, hOldFont;
-    RECT                        rRect_0, rRect_1;
-    PAINTSTRUCT                 psPainter;
-    static const wchar_t* const lpswzCaptions[] = { L"75", L"150", L"225", L"300", L"375", L"450", L"525", L"600", L"675" };
-    HDC                         hDeviceContext;
-    int                         till = 0, step = 0, full = 0;
+HINSTANCE        g_hinst;
+LRESULT          g_pos = 150;
 
-    switch (uMessage) {
+int WINAPI       wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow) {
+    HWND      hwnd;
+    MSG       msg;
+    WNDCLASSW wc     = { 0 };
+
+    wc.lpszClassName = L"Application";
+    wc.hInstance     = hInstance;
+    wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
+    wc.lpfnWndProc   = WndProc;
+    wc.hCursor       = LoadCursor(0, IDC_ARROW);
+
+    g_hinst          = hInstance;
+
+    RegisterClassW(&wc);
+    hwnd = CreateWindowW(
+        wc.lpszClassName, L"Burning control", WS_OVERLAPPEDWINDOW | WS_VISIBLE | WS_CLIPCHILDREN, 100, 100, 400, 250, 0, 0, hInstance, 0
+    );
+
+    while (GetMessage(&msg, NULL, 0, 0)) DispatchMessage(&msg);
+
+    return (int) msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    static HWND          hwndTrack, hwndBurn;
+    WNDCLASSW            rwc = { 0 };
+
+    INITCOMMONCONTROLSEX InitCtrlEx;
+
+    InitCtrlEx.dwSize = sizeof(INITCOMMONCONTROLSEX);
+    InitCtrlEx.dwICC  = ICC_BAR_CLASSES;
+    InitCommonControlsEx(&InitCtrlEx);
+
+    switch (msg) {
+        case WM_CREATE :
+
+            rwc.lpszClassName = L"BurningControl";
+
+            rwc.hbrBackground = GetSysColorBrush(COLOR_BTNFACE);
+            rwc.style         = CS_HREDRAW;
+            rwc.lpfnWndProc   = PanelProc;
+            rwc.hCursor       = LoadCursor(0, IDC_ARROW);
+            RegisterClassW(&rwc);
+
+            hwndBurn = CreateWindowExW(
+                WS_EX_STATICEDGE, L"BurningControl", NULL, WS_CHILD | WS_VISIBLE, 0, 330, 490, 30, hwnd, (HMENU) 1, NULL, NULL
+            );
+
+            hwndTrack = CreateWindowExW(
+                0,
+                TRACKBAR_CLASSW,
+                NULL,
+                WS_CHILD | WS_VISIBLE | TBS_FIXEDLENGTH | TBS_NOTICKS,
+                40,
+                25,
+                150,
+                25,
+                hwnd,
+                (HMENU) 2,
+                g_hinst,
+                NULL
+            );
+
+            SendMessage(hwndTrack, TBM_SETRANGE, TRUE, MAKELONG(0, 750));
+            SendMessage(hwndTrack, TBM_SETPAGESIZE, 0, 20);
+            SendMessage(hwndTrack, TBM_SETTICFREQ, 20, 0);
+            SendMessage(hwndTrack, TBM_SETPOS, TRUE, 150);
+            break;
+
+        case WM_SIZE : SetWindowPos(hwndBurn, NULL, 0, HIWORD(lParam) - 30, LOWORD(lParam), 30, SWP_NOZORDER); break;
+
+        case WM_HSCROLL :
+
+            g_pos = SendMessage(hwndTrack, TBM_GETPOS, 0, 0);
+            InvalidateRect(hwndBurn, NULL, TRUE);
+            break;
+
+        case WM_DESTROY : PostQuitMessage(0); break;
+    }
+
+    return DefWindowProcW(hwnd, msg, wParam, lParam);
+}
+
+LRESULT CALLBACK PanelProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    HBRUSH      hBrushYellow, hBrushRed, holdBrush;
+    HPEN        hPen, holdPen;
+    HFONT       hFont, holdFont;
+    PAINTSTRUCT ps;
+    RECT        rect, rect2;
+
+    wchar_t*    cap[] = { L"75", L"150", L"225", L"300", L"375", L"450", L"525", L"600", L"675" };
+
+    HDC         hdc;
+    int         till;
+    int         step, full;
+    int         i;
+
+    switch (msg) {
         case WM_PAINT :
-            hDeviceContext = BeginPaint(hWindow, &psPainter);
-            GetClientRect(hWindow, &rRect_0);
-            till         = rRect_0.right / 750.0L * rGlobalPosition;
-            step         = rRect_0.right / 10.0L;
-            full         = rRect_0.right / 750.0L * 700;
 
-            hYellowBrush = CreateSolidBrush(RGB(255, 255, 184));
-            hRedBrush    = CreateSolidBrush(RGB(255, 110, 110));
+            hdc = BeginPaint(hwnd, &ps);
+
+            GetClientRect(hwnd, &rect);
+
+            till         = (rect.right / 750.0) * g_pos;
+            step         = rect.right / 10.0;
+            full         = (rect.right / 750.0) * 700;
+
+            hBrushYellow = CreateSolidBrush(RGB(255, 255, 184));
+            hBrushRed    = CreateSolidBrush(RGB(255, 110, 110));
+
             hPen         = CreatePen(PS_NULL, 1, RGB(0, 0, 0));
-            hOldPen      = SelectObject(hDeviceContext, hPen);
+            holdPen      = SelectObject(hdc, hPen);
 
-            hFont        = CreateFontW(14, 0, 0, 0, FW_MEDIUM, 0, 0, 0, 0, 0, 0, 0, 0, L"Consolas");
-            hOldFont     = SelectObject(hDeviceContext, hFont);
+            hFont        = CreateFontW(13, 0, 0, 0, FW_MEDIUM, 0, 0, 0, 0, 0, 0, 0, 0, L"Tahoma");
+
+            holdFont     = SelectObject(hdc, hFont);
 
             if (till > full) {
-                SelectObject(hDeviceContext, hYellowBrush);
-                Rectangle(hDeviceContext, 0, 0, full, 30);
-                hOldBrush = SelectObject(hDeviceContext, hRedBrush);
-                Rectangle(hDeviceContext, full, 0, till, 0);
+                SelectObject(hdc, hBrushYellow);
+                Rectangle(hdc, 0, 0, full, 30);
+                holdBrush = SelectObject(hdc, hBrushRed);
+                Rectangle(hdc, full, 0, till, 30);
+
             } else {
-                hOldBrush = SelectObject(hDeviceContext, hYellowBrush);
-                Rectangle(hDeviceContext, 0, 0, till, 30);
+                holdBrush = SelectObject(hdc, hBrushYellow);
+                Rectangle(hdc, 0, 0, till, 30);
             }
 
-            SelectObject(hDeviceContext, hOldPen);
-            for (long long i = 0; i < 10; ++i) {
-                MoveToEx(hDeviceContext, i * step, 0, NULL);
-                LineTo(hDeviceContext, i * step, 7);
+            SelectObject(hdc, holdPen);
 
-                rRect_1.bottom = 28l;
-                rRect_1.top    = 8;
-                rRect_1.left   = i * step - 10;
-                rRect_1.left   = i * step + 10;
+            for (i = 1; i < 10; i++) {
+                MoveToEx(hdc, i * step, 0, NULL);
+                LineTo(hdc, i * step, 7);
 
-                SetBkMode(hDeviceContext, TRANSPARENT);
-                DrawTextW(hDeviceContext, lpswzCaptions[i - 1], wcsnlen_s(lpswzCaptions[i - 1], 5), &rRect_1, DT_CENTER);
+                rect2.bottom = 28;
+                rect2.top    = 8;
+                rect2.left   = i * step - 10;
+                rect2.right  = i * step + 10;
+
+                SetBkMode(hdc, TRANSPARENT);
+                DrawTextW(hdc, cap[i - 1], wcslen(cap[i - 1]), &rect2, DT_CENTER);
             }
 
-            SelectObject(hDeviceContext, hOldBrush);
-            DeleteObject(hYellowBrush);
-            DeleteObject(hRedBrush);
+            SelectObject(hdc, holdBrush);
+            DeleteObject(hBrushYellow);
+            DeleteObject(hBrushRed);
 
             DeleteObject(hPen);
 
-            SelectObject(hDeviceContext, hOldFont);
+            SelectObject(hdc, holdFont);
             DeleteObject(hFont);
 
-            EndPaint(hWindow, &psPainter);
+            EndPaint(hwnd, &ps);
             break;
-
-        default : break;
     }
 
-    return DefWindowProcW(hWindow, uMessage, wParameter, lParameter);
-}
-
-LRESULT CALLBACK WindowProcedure(_In_ HWND hWindow, _In_ UINT uMessage, _In_ WPARAM wParameter, _In_ LPARAM lParameter) {
-    static HWND          hTrack, hBurn;
-    WNDCLASSEXW          wcWindowClass;
-    INITCOMMONCONTROLSEX icCtrlInit;
-
-    wcWindowClass.cbSize = sizeof(WNDCLASSEXW);
-    icCtrlInit.dwSize    = sizeof(INITCOMMONCONTROLSEX);
-    icCtrlInit.dwICC     = ICC_BAR_CLASSES;
-    InitCommonControlsEx(&icCtrlInit);
-
-    switch (uMessage) {
-        case WM_CREATE :
-            {
-                wcWindowClass.lpszClassName = L"Burning Control";
-                wcWindowClass.hbrBackground = GetSysColorBrush(COLOR_BTNFACE);
-                wcWindowClass.style         = CS_HREDRAW | CS_VREDRAW;
-                wcWindowClass.hCursor       = LoadCursorW(NULL, IDC_ARROW);
-                wcWindowClass.lpfnWndProc   = PanelProcedure;
-                RegisterClassExW(&wcWindowClass);
-
-                hBurn = CreateWindowExW(
-                    WS_EX_STATICEDGE, L"Burning Control", NULL, WS_CHILD | WS_VISIBLE, 0, 330, 490, 30, hWindow, (HMENU) 1, NULL, NULL
-                );
-
-                hTrack = CreateWindowExW(
-                    WS_EX_OVERLAPPEDWINDOW | WS_EX_STATICEDGE,
-                    TRACKBAR_CLASSW,
-                    NULL,
-                    WS_CHILD | WS_VISIBLE | TBS_NOTICKS | TBS_FIXEDLENGTH,
-                    40,
-                    25,
-                    150,
-                    25,
-                    hWindow,
-                    (HMENU) 2,
-                    hGlobalInstance,
-                    NULL
-                );
-
-                SendMessageW(hTrack, TBM_SETRANGE, 1, MAKELONG(0, 750));
-                SendMessageW(hTrack, TBM_SETPAGESIZE, 0, 20);
-                SendMessageW(hTrack, TBM_SETTICFREQ, 20, 0);
-                SendMessageW(hTrack, TBM_SETPOS, 1, 150);
-                break;
-            }
-        case WM_SIZE :
-            {
-                SetWindowPos(hBurn, NULL, 0, HIWORD(lParameter) - 30, LOWORD(lParameter), 30, SWP_NOZORDER);
-                break;
-            }
-        case WM_HSCROLL :
-            {
-                rGlobalPosition = SendMessageW(hTrack, TBM_GETPOS, 0, 0);
-                InvalidateRect(hBurn, NULL, TRUE);
-                break;
-            }
-        case WM_DESTROY :
-            {
-                PostQuitMessage(0);
-                break;
-            }
-    }
-    return DefWindowProcW(hWindow, uMessage, wParameter, lParameter);
-}
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pwszCommandLine, int iCmdShow) {
-    HWND        hWindow;
-    MSG         msg;
-    WNDCLASSEXW wcexWindowdClass   = { 0 };
-
-    wcexWindowdClass.cbSize        = sizeof(WNDCLASSEXW);
-    wcexWindowdClass.lpszClassName = L"Application";
-    wcexWindowdClass.hInstance     = hInstance;
-    wcexWindowdClass.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
-    wcexWindowdClass.lpfnWndProc   = WindowProcedure;
-    wcexWindowdClass.hCursor       = LoadCursorW(0, IDC_ARROW);
-
-    hGlobalInstance                = hInstance;
-
-    RegisterClassExW(&wcexWindowdClass);
-    hWindow = CreateWindowW(
-        wcexWindowdClass.lpszClassName,
-        L"Burning control",
-        WS_OVERLAPPEDWINDOW | WS_VISIBLE | WS_CLIPCHILDREN,
-        100,
-        100,
-        400,
-        250,
-        0,
-        0,
-        hInstance,
-        0
-    );
-
-    while (GetMessageW(&msg, NULL, 0, 0)) DispatchMessageW(&msg);
-
-    return (int) msg.wParam;
+    return DefWindowProcW(hwnd, msg, wParam, lParam);
 }
