2a84a2f36cec8626b19d934e2fcff6dcc03a4551
anoban <2016sb117@univ.jfn.ac.lk>
2024-04-03 18:37:47 +0530

    modified:   .clang-tidy 	deleted:    DLLs/.vscode/launch.json 	deleted:    DLLs/.vscode/settings.json 	deleted:    DLLs/.vscode/tasks.json 	deleted:    DLLs/dllibrary.c 	new file:   DLLs/dynamic.c 	modified:   DLLs/main.c 	modified:   randoms.c

--- DLLs/main.c
+++ DLLs/main.c
@@ -4,11 +4,13 @@
 #include <stdlib.h>
 #include <Windows.h>
 
-// prototype for DLL routines
+// prototype for DLL routines, needed for load time linking
 extern long double __declspec(dllexport) __cdecl factorial(unsigned n);
 extern unsigned long long __stdcall gcd(const unsigned long long a, const unsigned long long b);
+extern const wchar_t* const greeting;
+extern const char* const    name;
 
-int main(void) {
+int                         main(void) {
     // load time linking
     const unsigned x = 5;
 
@@ -17,14 +19,19 @@ int main(void) {
     wprintf(L"gcd of %llu and %llu is %llu\n", a, b, gcd(a, b));
 
     // runtime linking
-    // for runtime linking, one does not need to link the export libraries.
-    const HMODULE hDLL                              = LoadLibraryExW(L"./dllibrary.dll", NULL, 0);
-    long double(__cdecl * ptrfactorial)(unsigned n) = GetProcAddress(hDLL, "factorial");
+    // for runtime linking, one does not need to link the import libraries.
+    const HMODULE hDLL                                   = LoadLibraryExW(L"./dynamic.dll", NULL, 0);
+    long double(__cdecl* const ptrfactorial)(unsigned n) = GetProcAddress(hDLL, "factorial");
+    unsigned long long(__stdcall* const ptrgcd)(unsigned long long, unsigned long long) = GetProcAddress(hDLL, "gcd");
+
+    // GetProcAddress basically does a series of strcmp searches on the DLL to find the requested procedure!
+
     if (ptrfactorial) wprintf_s(L"Factorial of %4u is %4.5Lf\n", x, (*ptrfactorial)(x));
-
-    unsigned long long(__stdcall * ptrgcd)(unsigned long long, unsigned long long) = GetProcAddress(hDLL, "gcd");
     if (ptrgcd) wprintf(L"gcd of %llu and %llu is %llu\n", a, b, (*ptrgcd)(a, b));
-    FreeLibrary(hDLL);
 
+    _putws(greeting); // relies on load time linking
+    puts(name);       // relies on load time linking
+
+    FreeLibrary(hDLL);
     return EXIT_SUCCESS;
 }
