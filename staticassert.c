be09efea268bb0ebd56437831cc73255cefd727d
anoban <2016sb117@univ.jfn.ac.lk>
2024-06-28 20:54:10 +0530

    deleted:     - Copy.clang-format 	deleted:     - Copy.clang-tidy 	deleted:     - Copy.clangd

---
+++ staticassert.c
@@ -0,0 +1,15 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+typedef struct node {
+        unsigned char byte;
+        unsigned      frequency;
+} node_t;
+
+static_assert(!offsetof(node_t, byte));
+static_assert(!false);
+static_assert(!0);
+
+int wmain(void) { return EXIT_SUCCESS; }
