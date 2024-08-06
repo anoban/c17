acc9259fea97dba4fb0eb03ebfe4861716bbcf8f
anoban <2016sb117@univ.jfn.ac.lk>
2024-08-06 07:21:52 +0530

    modified:   staticv.c

--- staticv.c
+++ staticv.c
@@ -1,14 +1,31 @@
-#include <stdio.h>
+// clang .\staticv.c -O3 -Wall -Wextra -pedantic -std=c23
 
-static inline unsigned __cdecl addone() {
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+static inline unsigned __cdecl increment() {
     static long count = 0;
     wprintf_s(L"count = %ld\n", count);
+    return ++count;
+}
 
-    count++;
-    return count;
+static inline const long double* __cdecl static_const_address() {
+    const static long double pie = M_PI; // works ;)
+    return &pie;
+}
+
+static inline long double* __cdecl static_address() {
+    static long double pie = M_PI; // works too ;)
+    return &pie;
 }
 
 int wmain(void) {
-    for (unsigned i = 0; i < 10; ++i) addone();
+    for (unsigned i = 0; i < 10; ++i) increment();
+    wprintf_s(L"Is that pi? %Lf\n", *static_const_address());
+
+    *static_address() *= 2.000;
+    wprintf_s(L"Is that 2 x pi? %Lf\n", *static_address()); // gotta love C <3
+
     return 0;
 }
