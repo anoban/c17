7a37fbc11f4b50cfc0aabceff57c5b9a93b2d852
anoban <2016sb117@univ.jfn.ac.lk>
2024-08-06 20:05:25 +0530

    modified:   stringliterals.c

--- stringliterals.c
+++ stringliterals.c
@@ -1,3 +1,5 @@
+// cl .\stringliterals.c /Wall /O2 /Ot /std:clatest /wd4710
+
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <stdio.h>
@@ -5,18 +7,20 @@
 
 double value = 0.000;
 
-static inline double __cdecl square(typeof(value) arg) { return arg * arg; }
+static inline double __cdecl square(typeof(double) arg) { return arg * arg; } // typeof really is decltype huh
+static inline double __cdecl cube(typeof(value) arg) { return arg * arg * arg; }
 
 int main(void) {
-    const char* const string                    = "STRING LITERAL";
-    typeof(string)    hello                     = "Hello there!How are ya doin?"; // typeof is poor man's decltype()
-    const typeof_unqual(string) const samsmith  = "For you............ I had to risk it aaaaaaaaaaall!";
+    char* const    string                 = "STRING LITERAL";
+    typeof(string) hello                  = "Hello there!How are ya doin?"; // typeof is poor man's decltype()
+    const typeof_unqual(string) samsmith  = "For you............ I had to risk it aaaaaaaaaaall!";
 
-    value                                      *= puts(string);
-    value                                      *= puts(hello);
-    value                                      *= puts(samsmith);
-    value                                      *= putchar("\nhalleloojah!"[0]);
-    value                                      *= printf_s("Square of pi is %Lf\n", square(M_PI));
+    value                                *= puts(string);
+    value                                *= puts(hello);
+    value                                *= puts(samsmith);
+    value                                *= putchar("\nhalleloojah!"[0]);
+    value                                *= printf_s("Square of pi is %F\n", square(M_PI));
+    value                                *= printf_s("Cube of pi is %F\n", cube(M_PI));
 
     return EXIT_SUCCESS;
 }
