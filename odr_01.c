8423eafbbd0dd4107a969a978c5e135142103e1f
anoban <2016sb117@univ.jfn.ac.lk>
2024-08-24 19:11:42 +0530

    modified:   odr_01.c 	modified:   odr_02.c

--- odr_01.c
+++ odr_01.c
@@ -7,7 +7,7 @@ extern __declspec(noinline) void __cdecl call(void); // definition in odr_02.c
 
 // static function
 static __declspec(noinline) double power(const double register base, const unsigned register exponent) {
-    _putws(L"static __declspec(noinline) double power(const double register base, const unsigned register exponent)");
+    _putws(__FILEW__ " static __declspec(noinline) double power(const double register base, const unsigned register exponent)");
     double result = base;
     for (unsigned i = 0; i < exponent; ++i) result *= base;
     return result;
@@ -16,7 +16,8 @@ static __declspec(noinline) double power(const double register base, const unsig
 unsigned expo = 12; // globally accessible
 
 int wmain(void) {
-    const long double res = power(M_PI, expo);
+    const long double res = power(M_PI, expo); // will call the power() function defined in this TU even though
+    // the non-static power() function defined in odr_02.c will be visible during the link time
     wprintf_s(L"%.6Lf raised to the power %u is %.6Lf\n", M_PI, expo, res);
     call();
     return EXIT_SUCCESS;
--- odr_02.c
+++ odr_02.c
@@ -4,13 +4,14 @@ extern unsigned expo;
 
 // non static function
 extern __declspec(noinline) long double __cdecl power(const double register base, const unsigned register exponent) {
-    _putws(L"extern __declspec(noinline) long double __cdecl power(const double register base, const unsigned register exponent)");
+    _putws(__FILEW__ " extern __declspec(noinline) long double __cdecl power(const double register base, const unsigned register exponent)"
+    );
     long double result = base;
     for (unsigned i = 0; i < exponent; ++i) result *= base;
     return result;
 }
 
 extern __declspec(noinline) void __cdecl call(void) {
-    expo = power(expo, 3);
+    expo = (unsigned) power(expo, 3); // will call the power() function defined in this TU
     wprintf_s(L"expo is now %u\n", expo);
 }
