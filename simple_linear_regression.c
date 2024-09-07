5f975eceb706254c14f16c346c560348fe7c6dc5
anoban <2016sb117@univ.jfn.ac.lk>
2024-09-07 18:19:59 +0530

    modified:   simple_linear_regression.c 	modified:   valist.c

--- simple_linear_regression.c
+++ simple_linear_regression.c
@@ -1,21 +1,20 @@
+#include <assert.h>
 #include <math.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-static const long double SLOPE         = 5.76415178263481;
-static const long double INTERCEPT     = -0.76415178263481;
-static const long double LEARNING_RATE = 0.15000;
-
-#define MAX_SIZE       20'000
-#define MAX_ITERATIONS 1'000
-#define RAND_MAXF      32767.0L // (long double) RAND_MAX
+static const unsigned long long MAX_SIZE       = 10'000;
+static const unsigned long long MAX_ITERATIONS = 1'000;
+static const long double        SLOPE          = 104.76415178263481;
+static const long double        INTERCEPT      = 2.76415178263481;
+static const long double        LEARNING_RATE  = 0.175000;
+static const long double        RAND_MAXF      = 32767.0L; // (long double) RAND_MAX
 
 static inline long double* __cdecl generate_x(_In_ register const size_t length) {
     long double* array = malloc(length * sizeof(long double));
     if (!array) {
-        //
+        fwprintf_s(stderr, L"memory allocation failure @ line %d inside function %s\n", __LINE__, __FUNCTIONW__);
         return NULL;
     }
     for (size_t i = 0; i < length; ++i) array[i] = rand() / RAND_MAXF;
@@ -30,15 +29,18 @@ static inline long double* __cdecl generate_y(
 ) {
     long double* array = malloc(length * sizeof(long double));
     if (!array) {
-        //
+        fwprintf_s(stderr, L"memory allocation failure @ line %d inside function %s\n", __LINE__, __FUNCTIONW__);
         return NULL;
     }
     for (size_t i = 0; i < length; ++i) array[i] = x[i] * a + b;
     return array;
 }
 
-static __forceinline long double __stdcall loss(
-    _In_ const long double const x, _In_ const long double const y, _In_ register const long double a, _In_ register const long double b
+static __forceinline long double __stdcall loss( // squared error / 2
+    _In_ register const long double x,
+    _In_ register const long double y,
+    _In_ register const long double a,
+    _In_ register const long double b
 ) {
     return powl(y - (a * x + b), 2.00L) / 2.0000L;
 }
@@ -58,7 +60,7 @@ static inline long double __stdcall cost(
 }
 
 // but the derivative of the cost function with respect to Y_hat is -(Y - Y_hat)
-// which makes all the squaring unnecessary!
+// which makes all the squaring operations unnecessary!
 
 typedef struct pair {
         long double value_0;
@@ -66,11 +68,11 @@ typedef struct pair {
 } pair_t;
 
 static inline pair_t __stdcall deriv_cost(
-    _In_count_(length) const long double* const x,
-    _In_count_(length) const long double* const y,
-    _In_ register const size_t                  length,
-    _In_ register const long double             a,
-    _In_ register const long double             b
+    _In_count_(length) register const long double* const x,
+    _In_count_(length) register const long double* const y,
+    _In_ register const size_t                           length,
+    _In_ register const long double                      a,
+    _In_ register const long double                      b
 ) {
     // derivative of the cost function with respect to the weight and bias (i.e. our prediction)
     long double dcost_w = 0.0000, dcost_b = 0.0000, dcost = 0.000;
@@ -85,22 +87,24 @@ static inline pair_t __stdcall deriv_cost(
 }
 
 int wmain(void) {
-    srand(time(NULL));
+    srand((unsigned) time(NULL));
 
     const long double* const predictor = generate_x(MAX_SIZE);
     const long double* const target    = generate_y(predictor, MAX_SIZE, SLOPE, INTERCEPT);
+    assert(predictor);
+    assert(target);
 
-    // boostrap a and b with two random values between 0 and 1.0
+    // boostrap a and b with two random values between 0.0 and 1.0
     long double learned_a = rand() / RAND_MAXF, learned_b = rand() / RAND_MAXF;
     wprintf_s(L"bootstrapped a = %2.10Lf, b = %2.10Lf\n", learned_a, learned_b);
-    pair_t d_error = { 0.000 };
+    pair_t derror = { 0.000 };
 
     // gradient descent
     for (size_t i = 0; i < MAX_ITERATIONS; ++i) {
-        d_error    = deriv_cost(predictor, target, MAX_SIZE, learned_a, learned_b);
         // calculate the derivatives and update learned_a and learned_b
-        learned_a -= d_error.value_0 * LEARNING_RATE;
-        learned_b -= d_error.value_1 * LEARNING_RATE;
+        derror     = deriv_cost(predictor, target, MAX_SIZE, learned_a, learned_b);
+        learned_a -= derror.value_0 * LEARNING_RATE;
+        learned_b -= derror.value_1 * LEARNING_RATE;
     }
 
     free(predictor);
--- valist.c
+++ valist.c
@@ -2,24 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// clang .\valist.c -Wall -Wextra -pedantic -O3 -std=cxx
-
 #define __TOSTR(EXPANDED_MACRO)     #EXPANDED_MACRO
 #define TO_STRING(UNEXPANDED_MACRO) __TOSTR(UNEXPANDED_MACRO)
 
-#if __STDC_VERSION__ >= 202311L
+#if __STDC_VERSION__ >= 202311L // C23 and later
     #pragma message(TO_STRING(__STDC_VERSION__))
 
-static inline double sumf(...) { // sum all the arguments, last argument must be 0
-    double sum      = 0.0;
-    char*  args_ptr = NULL;
+static inline double sumf(...) { // first argument must specify the number of args to sum
+    double  sum      = 0.0;
+    va_list args_ptr = NULL;
 
     // prior to C23, va_start() took a va_list and the expected argument count as arguments
     // since C23, va_start() evaluates only the first argument, i.e va_start() macro can be invoked with a single argument
     va_start(args_ptr); // will map to __va_start(&args_ptr, 0); with /std:c23 flag
 
                         // until we reach 0 (the last argument)
-    while (*((unsigned*) args_ptr)) sum += *(float*) (args_ptr += sizeof(float) - sizeof(float));
+    const unsigned argc = va_arg(args_ptr, unsigned);
+    for (unsigned i = 0; i < argc; ++i) sum += va_arg(args_ptr, float);
     args_ptr = NULL; // equivalent to  va_end(args_ptr)
     return sum;
 }
@@ -40,7 +39,7 @@ static inline double sumf(const unsigned long long argc, ...) { // sum all the a
 #endif
 
 int wmain(void) {
-    const double sixty = sumf(10.0F, 10.0F, 10.0F, 10.0F, 10.0F, 10.0F, 0U);
+    const double sixty = sumf(6, 10.0F, 10.0F, 10.0F, 10.0F, 10.0F, 10.0F);
     wprintf_s(L"sum is = %lf\n", sixty);
     return EXIT_SUCCESS;
 }
