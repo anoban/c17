e9e9ccfc1a83e5038ec24a35eec1e1da4d5fd08c
anoban <2016sb117@univ.jfn.ac.lk>
2025-01-25 07:35:16 +1100

    oh well

--- mm_shuffle_epi8.c
+++ mm_shuffle_epi8.c
@@ -4,20 +4,26 @@
 
 int wmain(void) {
 #if defined(__llvm__) && defined(__clang__)
-    static const __m128i BYTE_REVERSE_MASK = { 0x0C0D0E0F08090A0B, 0x0405060700010203 };
+    static const __v16qu MASK = {
+        // !!! WE WANT TO REVERSE THE PIXELS NOT THE BYTES, REVERSING PIXELS MEANS REVERSING CONSECUTIVE 4 BYTE BLOCKS
+        12, 13, 14, 15, 8, 9, 10, 11, 4, 5, 6, 7, 0, 1, 2, 3
+    };
+    __v16qu reversed;
 #elif defined(_MSC_VER) && defined(_MSC_FULL_VER)
-    static const __m128i BYTE_REVERSE_MASK = {
+    static const __m128i MASK = {
         // !!! WE WANT TO REVERSE THE PIXELS NOT THE BYTES, REVERSING PIXELS MEANS REVERSING CONSECUTIVE 4 BYTE BLOCKS
         .m128i_u8 = { 12, 13, 14, 15, 8, 9, 10, 11, 4, 5, 6, 7, 0, 1, 2, 3 }
     };
+    __m128i reversed;
 #endif
 
-    __m128i       reversed;
     unsigned char bytes[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    reversed              = _mm_shuffle_epi8(_mm_loadu_epi8(bytes), MASK);
 
-    reversed              = _mm_shuffle_epi8(_mm_loadu_epi8(bytes), BYTE_REVERSE_MASK);
-
+#ifdef __llvm__
+    for (unsigned i = 0; i < sizeof(__m128i); ++i) wprintf_s(L"%4d\n", reversed[i]);
+#elifdef _MSC_FULL_VER
     for (unsigned i = 0; i < sizeof(__m128i); ++i) wprintf_s(L"%4d\n", reversed.m128i_u8[i]);
-
+#endif
     return EXIT_SUCCESS;
 }
