040fd59bc0d99ddb2033e4fe59bec97b3f0e2157
anoban <2016sb117@univ.jfn.ac.lk>
2025-01-27 09:24:32 +1100

    modified:   sum.c

--- sum.c
+++ sum.c
@@ -2,9 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// prototype
-static long double __declspec(noinline) __stdcall sum_v2(const type _argtype, const unsigned _argc, ...);
-
 // i keep forgetting this!!! damn
 long double isum(const unsigned _argcount, /* variadic arguments */...) {
     long double total   = 0.0000;
@@ -90,6 +87,9 @@ static long double __declspec(noinline) __stdcall sum(const type _argtype, const
     return total;
 }
 
+// prototype
+static long double __declspec(noinline) __stdcall sum_v2(const type _argtype, const unsigned _argc, ...);
+
 int wmain(void) {
     wprintf_s(
         L"sum of 8,58,31,45,41,48,10,8,30,61,55,41,33,67,23,85,50,16,48,18 is %.4Lf\n",
@@ -138,44 +138,67 @@ int wmain(void) {
 
     wprintf(L"sum is %.5Lf\n", sum(FLOAT, 10, 27.00F, 66.00F, -31.00F, -15.00F, 84.00F, -100.00F, 58.00F, -128.00F, 78.00F, 113.00F));
     wprintf(L"sum is %.5Lf\n", sum(DOUBLE, 10, 27.00, 66.00, -31.00, -15.00, 84.00, -100.00, 58.00, -128.00, 78.00, 113.00));
+
+    ///////////////////////////////////////////////////////////////
+
+    _putws(L"\nV2\n");
+    wprintf(L"sum is %.5Lf\n", sum_v2(UCHAR, 10, 213UI8, 146UI8, 186UI8, 106UI8, 8UI8, 224UI8, 192UI8, 12UI8, 69UI8, 169UI8));
+    wprintf(L"sum is %.5Lf\n", sum_v2(CHAR, 10, 27I8, 66I8, -31I8, -15I8, 84I8, -100I8, 58I8, -128I8, 78I8, 113I8));
+    wprintf(L"sum is %.5Lf\n", sum_v2(USHORT, 10, 213UI16, 146UI16, 186UI16, 106UI16, 8UI16, 224UI16, 192UI16, 12UI16, 69UI16, 169UI16));
+    wprintf(L"sum is %.5Lf\n", sum_v2(SHORT, 10, 27I16, 66I16, -31I16, -15I16, 84I16, -100I16, 58I16, -128I16, 78I16, 113I16));
+    wprintf(L"sum is %.5Lf\n", sum_v2(ULONG, 10, 213LU, 146LU, 186LU, 106LU, 8LU, 224LU, 192LU, 12LU, 69LU, 169LU));
+    wprintf(L"sum is %.5Lf\n", sum_v2(LONG, 10, 27, 66, -31, -15, 84, -100, 58, -128, 78, 113));
+
+    wprintf(L"sum is %.5Lf\n", sum_v2(ULONGLONG, 10, 213LLU, 146LLU, 186LLU, 106LLU, 8LLU, 224LLU, 192LLU, 12LLU, 69LLU, 169LLU));
+    wprintf(L"sum is %.5Lf\n", sum_v2(LONGLONG, 10, 27LL, 66LL, -31LL, -15LL, 84LL, -100LL, 58LL, -128LL, 78LL, 113LL));
+    wprintf(L"sum is %.5Lf\n", sum_v2(FLOAT, 10, 27.00F, 66.00F, -31.00F, -15.00F, 84.00F, -100.00F, 58.00F, -128.00F, 78.00F, 113.00F));
+    wprintf(L"sum is %.5Lf\n", sum_v2(DOUBLE, 10, 27.00, 66.00, -31.00, -15.00, 84.00, -100.00, 58.00, -128.00, 78.00, 113.00));
+
     return EXIT_SUCCESS;
 }
 
 static long double __declspec(noinline) __stdcall sum_v2(const type _argtype, const unsigned _argc, ...) {
     long double total = 0.000;
+    static_assert(sizeof(int) == sizeof(long));           // WIN32
+    static_assert(sizeof(double) == sizeof(long double)); // WIN32
 
     switch (_argtype) {
-        // unsigned type group subject to type promotion
+        // type group subject to type promotion to int (long)
+        case CHAR   : [[fallthrough]];
         case UCHAR  : [[fallthrough]];
+        case SHORT  : [[fallthrough]];
         case USHORT : [[fallthrough]];
-        case ULONG :
-            {
-                const unsigned long* _ptr = NULL;
-                __va_start(&_ptr, _argc);
-                for (unsigned i = 0; i < _argc; ++i) total += _ptr[i];
-                break;
-            }
-            // signed type group subject to type promotion
-        case CHAR  : [[fallthrough]];
-        case SHORT : [[fallthrough]];
         case LONG :
-            {
-                const long* _ptr = NULL;
-                __va_start(&_ptr, _argc);
-                for (unsigned i = 0; i < _argc; ++i) total += _ptr[i];
-                break;
-            }
-
-        case ULONGLONG :
-            for (unsigned i = 0; i < _argc; ++i)
-                total += ((unsigned long long*) _ptr)[i]; // NOLINT(cppcoreguidelines-narrowing-conversions)
-            break;
+        //    {
+        //        const long* _ptr = NULL;
+        //        __va_start(&_ptr, _argc);
+        //        for (unsigned i = 0; i < _argc; ++i) total += _ptr[i];
+        //        break;
+        //    }
+        case ULONG :
+            //    {
+            //        const unsigned long* _ptr = NULL; // all the above unsigned types w
+            //        __va_start(&_ptr, _argc);
+            //        for (unsigned i = 0; i < _argc; ++i) total += _ptr[i];
+            //        break;
+            //    }
 
         case LONGLONG :
-            for (unsigned i = 0; i < _argc; ++i) total += ((long long*) _ptr)[i]; // NOLINT(cppcoreguidelines-narrowing-conversions)
-            break;
+            {
+                const long long* _ptr = NULL;
+                __va_start(&_ptr, _argc);
+                for (unsigned i = 0; i < _argc; ++i) total += _ptr[i]; // NOLINT(cppcoreguidelines-narrowing-conversions)
+                break;
+            }
+        case ULONGLONG :
+            {
+                const unsigned long long* _ptr = NULL;
+                __va_start(&_ptr, _argc);
+                for (unsigned i = 0; i < _argc; ++i) total += _ptr[i]; // NOLINT(cppcoreguidelines-narrowing-conversions)
+                break;
+            }
 
-        case FLOAT : [[fallthrough]];
+        case FLOAT : [[fallthrough]]; // floats will be promoted to doubles
         case DOUBLE :
             {
                 const double* _ptr = NULL;
